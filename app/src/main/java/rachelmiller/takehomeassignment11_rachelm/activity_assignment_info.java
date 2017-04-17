bbe44d073b3c60da07727882624c4b0656ad6125
rachelmiller <rkm2314@tc.columbia.edu>
2017-04-17 15:44:09 -0400

    xml

---
+++ app/src/main/java/rachelmiller/takehomeassignment11_rachelm/activity_assignment_info.java
@@ -0,0 +1,13 @@
+package rachelmiller.takehomeassignment11_rachelm;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class activity_assignment_info extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_assignment_info);
+    }
+}
